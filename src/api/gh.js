5d15980f869b9c327bad30b6bd68bc97d0ca891d
Antoni <ant.morawski@gmail.com>
2020-04-30 23:44:57 +0200

    styling refactor

--- src/api/gh.js
+++ src/api/gh.js
@@ -3,7 +3,3 @@ import axios from "axios"
 export default axios.create({
   baseURL: "https://api.github.com",
 })
-
-// let headers = new Headers()
-// headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-// headers.append('Access-Control-Allow-Credentials', 'true');
